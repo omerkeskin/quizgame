4f8a35d8200cebc60637a820248c7ed583cc1df7
omerkeskin <omer.keskin.deu@hotmail.com>
2020-04-05 15:07:03 +0300

    Solution, Part 1

--- script.js
+++ script.js
@@ -21,3 +21,48 @@ c) correct answer (I would use a number for this)
 
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
+
+(function () {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    Question.prototype.displayQuestion = function () {
+        console.log(this.question);
+        for (var  i=0; i < this.answers.length ; i++){
+            console.log(i+'. '+ this.answers[i]);
+        }
+    }
+
+    Question.prototype.checkAnswer = function (chosenElement) {
+        if(this.correct === chosenElement){
+            console.log('Correct answer.');
+        }else{
+            console.log('Wrong answer. Try again :)');
+        }
+    }
+
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+        ['Yes', 'No'],
+        0);
+
+    var q2 = new Question('What is the name of this course\'s teacher?',
+        ['John', 'Micheal', 'Jonas'],
+        2);
+
+    var q3 = new Question('What does best describe coding?',
+        ['Boring', 'Hard', 'Fun', 'Tediuos'],
+        2);
+
+    var questions = [q1, q2, q3];
+
+    var selectedQueryIndex = Math.floor(Math.random() * questions.length);
+    questions[selectedQueryIndex].displayQuestion();
+
+    var answer = parseInt(prompt('Please select the correct answer.')) ;
+    questions[selectedQueryIndex].checkAnswer(answer);
+})();
+
+
